550bd22448276eb1f981f234ac8126731ee3054f
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2024-03-05 06:22:07 +0100

    fixed scrollable card scroll effect

--- app/static/js/analytics.js
+++ app/static/js/analytics.js
@@ -1,7 +1,43 @@
 const carousel = document.querySelector(".ana-scroll-card");
 
-const dragging = (e) => {
-    carousel.scrollLeft = e.pageX;
+const arrowBtns = document.querySelectorAll(".flexy1 i");
+
+function getElementWidth(element) {
+  let card = document.querySelector(element);
+  let styles = window.getComputedStyle(card);
+  let width = styles.getPropertyValue("width");
+  return parseFloat(width);
 }
 
-carousel.addEventListener ("mousemove", dragging);
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    let scrollElementWidth = getElementWidth(".scroll-element");
+    carousel.scrollLeft +=
+      btn.id === "left" ? -scrollElementWidth : scrollElementWidth;
+  });
+});
+
+let isMouseDown = false,
+  startX,
+  startScrollLeft;
+
+const mouseDown = (e) => {
+  isMouseDown = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const mouseUp = () => {
+  isMouseDown = false;
+  carousel.classList.remove("dragging");
+};
+
+const dragging = (e) => {
+  if (!isMouseDown) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+document.addEventListener("mouseup", mouseUp);
+carousel.addEventListener("mousedown", mouseDown);
+carousel.addEventListener("mousemove", dragging);
