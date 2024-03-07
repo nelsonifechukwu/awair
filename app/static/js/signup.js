2777c04e2e92cefbb96fef5c943c860bcb94cac1
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2024-03-07 16:27:46 +0100

    updated map request

--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -1,18 +1,4 @@
-// login and signup page
-// let signup = document.querySelector(".signup");
-// let login = document.querySelector(".login");
-// let slider = document.querySelector(".slider");
-// let formSection = document.querySelector(".form-section");
 
-// signup.addEventListener("click", () => {
-// 	slider.classList.add("moveslider");
-// 	formSection.classList.add("form-section-move");
-// });
-
-// login.addEventListener("click", () => {
-// 	slider.classList.remove("moveslider");
-// 	formSection.classList.remove("form-section-move");
-// });
 
 // password view and hide
 const togglePassword = document
