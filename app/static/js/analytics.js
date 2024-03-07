cba69ff5dd87f5afaa90fd3e095df61b14ba7c72
nelsonifechukwu <nelson.ifechukwu@gmail.com>
2024-03-07 16:25:02 +0100

    added time delay to update scrollLeft after click eventListener execution

--- app/static/js/analytics.js
+++ app/static/js/analytics.js
@@ -1,21 +1,29 @@
 const carousel = document.querySelector(".ana-scroll-card");
-
 const arrowBtns = document.querySelectorAll(".flexy1 i");
+const lbutton = document.getElementById("button-left");
+let scrollElementWidth = getElementWidth(".scroll-element");
 
 function getElementWidth(element) {
   let card = document.querySelector(element);
   let styles = window.getComputedStyle(card);
   let width = styles.getPropertyValue("width");
-  return parseFloat(width);
+  return parseInt(width);
 }
-
 arrowBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    let scrollElementWidth = getElementWidth(".scroll-element");
+  btn.onclick = () => {
     carousel.scrollLeft +=
-      btn.id === "left" ? -scrollElementWidth : scrollElementWidth;
-  });
+      btn.id === "button-left" ? -scrollElementWidth : scrollElementWidth;
+    carousel.scrollLeft < 0 ? (carousel.scrollLeft = 0) : 0;
+    // console.log(`SL1${carousel.scrollLeft}`);
+    setTimeout(buttonState, 255); //Delay buttonState call to allow .scrollLeft to update after the eventlistener is triggered
+  };
 });
+
+let buttonState = () => {
+  parseInt(carousel.scrollLeft) > 0
+    ? (lbutton.style.display = "block")
+    : (lbutton.style.display = "none");
+};
 
 let isMouseDown = false,
   startX,
@@ -41,3 +49,4 @@ const dragging = (e) => {
 document.addEventListener("mouseup", mouseUp);
 carousel.addEventListener("mousedown", mouseDown);
 carousel.addEventListener("mousemove", dragging);
+document.body.addEventListener("click", buttonState);
